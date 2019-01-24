519f01ca4b2356f62b0e3ef54844e6897c9f5980
Higherings <lizar@igarcia.datum.com.gt>
2019-01-24 17:02:59 -0600

    Prevents creating a snapshot if is already creating one

--- shotter/shotter.py
+++ shotter/shotter.py
@@ -17,6 +17,10 @@ def filter_instances(proyecto):
         instances = ec2.instances.all()
     return instances
 
+def has_pending_snapshot(volumen):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group() #Will group volumes and instances groups
 def cli():
     """Shotter manages snapshots"""
@@ -72,9 +76,14 @@ def create_snapshots(proyecto):
         print("Stopping {0}...".format(i.id))
         i.stop()
         i.wait_until_stopped()  #waits until the instance is stopped
+
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print("     Skipping {0}, snapshot already in progress".format(v.id))
+                continue
             print("     Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Shotter Snaps")
+
         print("Starting {0}...".format(i.id))
         i.start()
         i.wait_until_running()
