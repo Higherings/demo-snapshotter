c6747cb313d48e85cc036b324ed623bbacaed3bc
Higherings <lizar@igarcia.datum.com.gt>
2019-01-24 16:40:21 -0600

    Create snapshot implemented

--- shotter/shotter.py
+++ shotter/shotter.py
@@ -1,3 +1,5 @@
+##HOW-TO run: pipenv run python shotter/shotter.py instances snapshot --proyecto=python_demo
+
 import boto3
 import click    #module to work with parameters
 #import sys #for arguments as input
@@ -63,10 +65,17 @@ def create_snapshots(proyecto):
     "Create Snapshots"    #doc string
     instances = filter_instances(proyecto)
     for i in instances:
-        #i.stop()
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()  #waits until the instance is stopped
         for v in i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
+            print("     Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Shotter Snaps")
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("All done!")
     return
 
 
